155821f0635592d48d6fff147f266c48249e07cb
Snippyyy <Snippy_06@hotmail.com>
2024-11-24 15:01:43 +0100

    6º  - USERS - Factories y Seeders
    -Se ha actualizado el factory de los usuarios añadiendo los nuevos campos a generar, además, se ha hecho uso de la api de <Dicebear> para generar imágenes de usuario personalizadas en svg, generadas con el hash md5 en el email generado de cada usuario. haciendo que este sea único para cada uno.
    
    -Se ha creado un UserSeeder que se ira actualizando según las necesidades, por defecto genera 10 usuarios

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Services\ProvinceService;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -23,9 +24,15 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $provinces = ProvinceService::getAll();
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'address' => fake()->address(),
+            'postal_code' => fake()->postcode(),
+            'phone_number' => fake()->phoneNumber(),
+            'province' => $provinces[array_rand($provinces)],
+            'email' => $email = fake()->unique()->safeEmail(),
+            'avatar' => 'https://api.dicebear.com/5.x/avataaars/svg?seed=' . md5($email),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
