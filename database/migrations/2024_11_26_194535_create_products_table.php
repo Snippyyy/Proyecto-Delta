cf778b4a8b397d6f75364eb6efa69baf61d45861
Snippy <168076645+Snippyyy@users.noreply.github.com>
2025-01-02 23:27:48 +0100

    27 º - Corrección en el borrado de categorías
    Al borrar categorias que tuvieran productos, a nivel de base de datos, no se permitia por la relación existente, se ha modificado la migración de las categorias para que al borrar una categoria se asigne nulo en el category id de los productos asociados, de esta manera los productos seguiran apareciendo en la vista general de todos los productos, es decir, la pagina principal, sin embargo, no pertenecerán a ninguna categoria hasta que el usuario autor vuelva a asignársela en la pagina de edicion del producto

--- database/migrations/2024_11_26_194535_create_products_table.php
+++ database/migrations/2024_11_26_194535_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->foreignId('seller_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('category_id')->constrained('categories');
+            $table->foreignId('category_id')->nullable()->constrained('categories')->nullOnDelete();
             $table->string('name');
             $table->string('description');
             $table->decimal('price');
