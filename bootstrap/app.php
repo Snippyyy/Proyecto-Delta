316a178611951faba690e8c5edf391bd623eb751
Snippyyy <Snippy_06@hotmail.com>
2025-02-18 01:16:37 +0100

    61 º - Excepcion Global para API
    Se ha añadido en app.php el manejo de excepción en caso de error 404 para que el usuario reciba una respuesta especifica

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,9 @@ use App\Http\Middleware\AlwaysAcceptJsonMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use \Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -23,5 +26,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->prependToGroup('api', AlwaysAcceptJsonMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (NotFoundHttpException $e, Request $request) {
+            if ($request->wantsJson()) {
+                return response()->json(['message' => 'Object Not Found'], 404);
+            }
+        });
     })->create();
